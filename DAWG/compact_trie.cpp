863b9c1bac5a114b8ba6d58b78686cbff33542a0
Sophie Crowley <crowleso@tcd.ie>
2021-03-18 15:28:49 +0000

    Functioning binary read/write

--- DAWG/compact_trie.cpp
+++ DAWG/compact_trie.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <iterator>
+#include <cmath>
 #include "compact_trie.h"
 
 using namespace std;
@@ -28,12 +29,14 @@ CompactTrie::CompactTrie(string fileName, bool compacted)
 
 void CompactTrie::createCode()
 {
-    for(int i=1; i<=256; i+=64) 
+    for (int i = 1; i <= 256; i += 64)
     {
         int index = i;
-        for(char l = 'a'; l<='z'; ++l) {
+        for (char l = 'a'; l <= 'z'; ++l)
+        {
             numToChar[index] = l;
-            if(i==1) {
+            if (i == 1)
+            {
                 charToNum[l] = index;
             }
             index++;
@@ -125,27 +128,58 @@ bool CompactTrie::doesWordExist(string word)
 
 void CompactTrie::writeToFile(string fileName)
 {
-    std::ofstream outfile;
+    ofstream outfile;
     outfile.open(fileName, ios::out | ios::binary);
+    unsigned int listSize = branchList.size();
+    outfile.write(reinterpret_cast<char *>(&listSize), sizeof(int));
     for (auto it = branchList.begin(); it != branchList.end(); ++it)
     {
         unsigned char letter = charToNum.at(it->first.first);
-        if(it->second) {
-          letter+=64;  
+        if (it->second)
+        {
+            letter += 64;
         }
         int index = it - branchList.begin();
-        if(nodeList.at(index).second == true) {
+        if (nodeList.at(index).second == true)
+        {
             letter += 128;
         }
-        unsigned int frequency = it->first.second;
         outfile.write((char *)(&letter), sizeof(letter));
-        outfile.write((char *)(&frequency), sizeof(frequency));
+        unsigned int frequency = it->first.second;
+        writeInteger(frequency, &outfile);
     }
-    outfile << "\n";
     for (auto it = nodeList.begin(); it != nodeList.end(); ++it)
     {
         unsigned int index = it->first;
-        outfile.write((char *)(&index), sizeof(index));
+        writeInteger(index, &outfile);
+    }
+}
+
+void CompactTrie::writeInteger(unsigned int index, ofstream *outfile)
+{
+    unsigned char curr;
+    if (index < 128)
+    {
+        curr = index;
+        outfile->write((char *)(&curr), sizeof(curr));
+    }
+    else
+    {
+        curr = ((index % 128) + 128);
+        outfile->write((char *)(&curr), sizeof(curr));
+        index /= 128;
+        if (index < 128)
+        {
+            curr = index;
+            outfile->write((char *)(&curr), sizeof(curr));
+        }
+        else
+        {
+            curr = ((index % 128) + 128);
+            outfile->write((char *)(&curr), sizeof(curr));
+            curr = (index / 128);
+            outfile->write((char *)(&curr), sizeof(curr));
+        }
     }
 }
 
@@ -153,55 +187,89 @@ void CompactTrie::readFromFile(string fileName)
 {
     std::ifstream infile;
     infile.open(fileName, ios::in | ios::binary);
+    unsigned int listSize = 0;
+    infile.read(reinterpret_cast<char *>(&listSize), sizeof(int));
     unsigned char curr;
-    infile.read((char *)(&curr), sizeof(curr));
     vector<bool> terminality;
-    while (curr != '\n')
+    for (int j = 0; j < listSize; j++)
     {
-        unsigned char letter;
+        infile.read((char *)(&curr), sizeof(curr));
         bool onlyDaughter = false;
-        unsigned int frequency = 0;
-        letter = numToChar[int(curr)];
-        if(int(curr)>64) {
+        unsigned char letter = numToChar[int(curr)];
+        if (int(curr) > 64)
+        {
             onlyDaughter = true;
         }
-        if(int(curr)> 128) {
+        if (int(curr) > 128)
+        {
             terminality.push_back(true);
         }
-        else {
+        else
+        {
             terminality.push_back(false);
         }
-        infile.read(reinterpret_cast<char *>(&curr), sizeof(int));
-        frequency += curr;
-        branchList.push_back(pair<pair<char,int>,bool>(pair<char,int>(letter, frequency), onlyDaughter));
-        infile.read((char *)(&curr), sizeof(curr));
+        infile.read((char *)(&curr), sizeof(char));
+        unsigned int frequency = getIntegerVal(&infile, curr);
+        branchList.push_back(pair<pair<char, int>, bool>(pair<char, int>(letter, frequency), onlyDaughter));
     }
     int i = 0;
-    while (infile.read(reinterpret_cast<char *>(&curr), sizeof(int)))
+    while (infile.read((char *)(&curr), sizeof(char)))
     {
-        unsigned int index = 0;
-        index += curr;
         bool terminal = terminality[i];
         i++;
-        nodeList.push_back(pair<int,bool>(index, terminal));
+        int index = getIntegerVal(&infile, curr);
+        nodeList.push_back(pair<int, bool>(index, terminal));
     }
 }
 
-void CompactTrie::displayLists() {
-    for(auto it = branchList.begin(); it != branchList.end(); ++it)
+int CompactTrie::getIntegerVal(ifstream *infile, unsigned char curr)
+{
+    int index = 0;
+    if (curr <= 127)
     {
-        cout << "(" << it->first.first << "," << it->second << ") ";
+        index += curr;
     }
-    cout << endl;
-    for(auto it = nodeList.begin(); it != nodeList.end(); ++it)
+    else
     {
-        cout << "(" << it->first << "," << it->second << ") ";       
+        unsigned char secondChar;
+        unsigned char thirdChar;
+        int remainingBits = (int)pow(2, 14);
+        index += (curr - 128);
+        infile->read((char *)(&secondChar), sizeof(secondChar));
+        if (secondChar > 127)
+        {
+            index += ((secondChar - 128) * 128);
+            infile->read((char *)(&thirdChar), sizeof(thirdChar));
+            index += (thirdChar * remainingBits);
+        }
+        else
+        {
+            index += (secondChar * 128);
+        }
+    }
+    return index;
+}
+
+void CompactTrie::displayLists()
+{
+    cout << "branchList" << endl;
+    for (auto it = branchList.begin(); it != branchList.end(); ++it)
+    {
+        cout << "(" << it->first.first << "," << it->second << ","
+             << "f=" << it->first.second << ") ";
+    }
+    cout << endl << "nodeList" << endl;
+    for (auto it = nodeList.begin(); it != nodeList.end(); ++it)
+    {
+        cout << "(" << it->first << "," << it->second << ") ";
     }
 }
 
 int main(int argc, char *argv[])
 {
     CompactTrie compactTrie = CompactTrie(argv[1], false);
+    compactTrie.displayLists();
+    cout << endl << endl;
     compactTrie.writeToFile("compact.txt");
     CompactTrie compactTrie2 = CompactTrie("compact.txt", true);
     compactTrie2.displayLists();
--- DAWG/compact_trie.h
+++ DAWG/compact_trie.h
@@ -13,19 +13,21 @@ public:
     CompactTrie(string fileName, bool compacted);
     bool doesWordExist(string word);
     void processTrie(Trie t);
-    void processNode(Node* n, bool lastBranch);
+    void processNode(Node *n, bool lastBranch);
     void writeToFile(string fileName);
+    void writeInteger(unsigned int index, ofstream *outfile);
+    int getIntegerVal(ifstream *infile, unsigned char curr);
     void readFromFile(string fileName);
     void createCode();
     void displayLists();
 
 protected:
     int counter;
-    vector<pair<pair<char,int>,bool>> branchList;
-    vector<pair<int,bool>> nodeList;
-    queue<Node*> nodes;
-    map<int,char> numToChar;
-    map<char,int> charToNum;
+    vector<pair<pair<char, int>, bool>> branchList;
+    vector<pair<int, bool>> nodeList;
+    queue<Node *> nodes;
+    map<int, char> numToChar;
+    map<char, int> charToNum;
 };
 
 #endif // COMPACT_TRIE_H

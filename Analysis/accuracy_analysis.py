a989db16d425e71851fc9961cca16390ae172b31
Sophie Crowley <crowleso@tcd.ie>
2021-04-12 13:55:53 +0100

    Update accuracy_analysis.py

--- Analysis/accuracy_analysis.py
+++ Analysis/accuracy_analysis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+# Gets average difference between pairs in columns.
 def average_difference(column1, column2):
     sum = 0
     for val1, val2, in zip(column1, column2):
@@ -17,10 +18,12 @@ def main():
     print(df[['Original','Log with 4 places', 'Log with 3 places', 'Log with 2 places']].describe())
 
     for column in columns:
+        # Checking distribution of data.
         # print(stats.shapiro(df['Original']))
         # print(stats.shapiro(df[column]))
         # plt.hist(df[column], range=(0,0.00005))
 
+        # Getting Wilcoxon results.
         result = stats.wilcoxon(df['Original'], df[column])
         print(result)
         alpha = 0.05
